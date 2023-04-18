11e9ecf35732fd8ef5a2bf92923da70e553802e3
Ty <129621531+notTyTy@users.noreply.github.com>
2023-04-19 00:00:49 +0800

    formatted and cleaned up the code
    - No more JsonSerializer.Deserialize everywhere :)
    - No more empty lines 
    -  Just looks better :D

--- Program.cs
+++ Program.cs
@@ -4,22 +4,19 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using System.Collections.Generic;
 
-
 class Program
 {
     static async Task Main()
     {
         HttpClient httpClient = new HttpClient();
         string apiUrl = "https://api.hypixel.net/skyblock/bazaar";
-            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
 
             if (response.IsSuccessStatusCode)
             {
-
                 string content = await response.Content.ReadAsStringAsync();
-                    JsonSerializer.Deserialize<bazaar_response>(content);
-                    bazaar_response data = JsonSerializer.Deserialize<bazaar_response>(content);
-                    bazaar_return(data);
+                bazaar_response data = JsonSerializer.Deserialize<bazaar_response>(content);
+                bazaar_return(data);
             }
             else
             {
@@ -29,7 +26,7 @@ class Program
     static string user_return()
     {
         Console.Write("Please input a product type: "); // User Input
-        string user_input = Console.ReadLine().ToUpper().Replace(" ", "_"); // converts user input to readable JSon format
+        string user_input = Console.ReadLine().ToUpper().Replace(" ", "_"); // converts user input to readable JSON format
         if (user_input == "POTATO" || user_input == "CARROT")
         { 
             user_input += "_ITEM";
@@ -40,27 +37,27 @@ class Program
     {               
 
         string user_input = user_return();
-        Console.WriteLine(JsonSerializer.Serialize("Current Sell Price: " + data.products[user_input].quick_status.sellPrice));
-        Console.WriteLine(JsonSerializer.Serialize("Current Sell Volume: " + data.products[user_input].quick_status.sellVolume));
-        Console.WriteLine(JsonSerializer.Serialize("Current Sell Orders: " + data.products[user_input].quick_status.sellOrders));
-        Console.WriteLine(JsonSerializer.Serialize("Current Buy Price: " + data.products[user_input].quick_status.buyPrice));
-        Console.WriteLine(JsonSerializer.Serialize("Current Buy Orders: " + data.products[user_input].quick_status.buyOrders));
+
+        Console.WriteLine("Current Sell Price: " + data.products[user_input].quick_status.sellPrice);
+        Console.WriteLine("Current Sell Volume: " + data.products[user_input].quick_status.sellVolume);
+        Console.WriteLine("Current Sell Orders: " + data.products[user_input].quick_status.sellOrders);
+        Console.WriteLine("Current Buy Price: " + data.products[user_input].quick_status.buyPrice);
+        Console.WriteLine("Current Buy Orders: " + data.products[user_input].quick_status.buyOrders);
     }
 }
-
 public class bazaar_response{
     public bool success {get; set;}
     public long lastUpdated {get; set;}
-    public Dictionary<string, product> products {get; set;}
+    public Dictionary<string, Product> products {get; set;}
 }
-public class product
+public class Product
 {
     public string product_id {get; set;}
-    public product_status quick_status {get; set;}
-    public product_sell_summary[] sell_summary {get; set;}
-    public product_buy_summary[] buy_summary {get; set;}
+    public Product_status quick_status {get; set;}
+    public Product_sell_summary[] sell_summary {get; set;}
+    public Product_buy_summary[] buy_summary {get; set;}
 }
-public class product_status
+public class Product_status
 {
     public string productId {get; set;}
     public float sellPrice {get; set;}
@@ -72,13 +69,14 @@ public class product_status
     public int buyOrders {get; set;}
 }
 
-public class product_sell_summary 
+public class Product_sell_summary 
 {
     public int amount {get; set;}
     public float pricePerUnit {get; set;}
     public int orders {get; set;}
 }
-public class product_buy_summary
+public class
+Product_buy_summary
 {
     public int amount {get; set;}
     public float pricePerUnit {get; set;}
